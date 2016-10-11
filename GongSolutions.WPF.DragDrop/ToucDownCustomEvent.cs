e889cf22b4249448f77a5d7c92b91bda3140c629
injector <injector@DESKTOP-5G1QQKJ>
2016-10-11 10:35:33 -0400

    Add project files.

---
+++ GongSolutions.WPF.DragDrop/ToucDownCustomEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace GongSolutions.WPF.DragDrop
+{
+   public class TouchDownCustomEvent : UIElement
+    {
+        // Create a custom routed event by first registering a RoutedEventID
+        // This event uses the bubbling routing strategy
+        public static readonly RoutedEvent TapEvent = EventManager.RegisterRoutedEvent(
+            "Tap", RoutingStrategy.Direct, typeof(RoutedEventHandler), typeof(TouchDownCustomEvent));
+
+        // Provide CLR accessors for the event
+        public event RoutedEventHandler Tap
+        {
+            add { AddHandler(TapEvent, value); }
+            remove { RemoveHandler(TapEvent, value); }
+        }
+
+        // This method raises the Tap event
+        void RaiseTapEvent()
+        {
+            RoutedEventArgs newEventArgs = new RoutedEventArgs(TapEvent);
+            RaiseEvent(newEventArgs);
+        }
+        // For demonstration purposes we raise the event when the MyButtonSimple is clicked
+        protected void OnClick()
+        {
+            RaiseTapEvent();
+        }
+    }
+}
---
+++ Showcase.WPF.DragDrop.NET45/Views/Issues.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+#if !NET35
+using System.Threading.Tasks;
+#endif
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Showcase.WPF.DragDrop.Views
+{
+    /// <summary>
+    /// Interaction logic for Issues.xaml
+    /// </summary>
+    public partial class Issues : UserControl
+    {
+        public Issues()
+        {
+            InitializeComponent();
+        }
+
+        private void UIElement_OnLostTouchCapture(object sender, TouchEventArgs e)
+        {
+
+        }
+
+        private void UIElement_OnTouchLeave(object sender, TouchEventArgs e)
+        {
+            Debug.WriteLine("touch leave ");
+
+        }
+
+        private void UIElement_OnTouchEnter(object sender, TouchEventArgs e)
+        {
+            Debug.WriteLine("touch enter ");
+         }
+    }
+}
